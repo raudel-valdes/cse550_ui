7f506cbae602f174ef52ca02315aba26ef7224eb
Raudel Valdes <raudel.valdes2015@gmail.com>
2020-11-30 08:22:21 -0500

    changes

--- interface.py
+++ interface.py
@@ -5,16 +5,18 @@ import os
 
 UPLOAD_FOLDER = './uploaded_images'
 PROCESSED_FOLDER = './processed_images'
+TEMPLATES_FOLDER = './static'
 
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['PROCESSED_FOLDER'] = PROCESSED_FOLDER
+app.config['TEMPLATES_FOLDER'] = TEMPLATES_FOLDER
 
-@app.route('/uploadImage')
+@app.route('/')
 def index():
-    return render_template('Interface.html')
+    return render_template('interface.html')
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -24,48 +26,65 @@ def file_type(filename):
     return '.' in filename and \
         filename.rsplit('.',1)[1].lower()
 
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
+@app.route('/uploadImage', methods=['GET','POST'])
+def uploadImage():
     if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
+        if 'file' in request.files:
             flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('sendImage', filename=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''                                 
+            file = request.files['file']
+
+            if file.filename != '':
+                flash('No selected file')
+
+                if file and allowed_file(file.filename):
+                    filename = secure_filename(file.filename)
+                    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                    return redirect(url_for('sendImage', filename=filename))
+
+    return "Error"
+    
 @app.route('/sendImage/<filename>')
 def sendImage(filename):
     fileType = file_type(filename)
     serverMethods = xmlrpc.client.ServerProxy('http://ec2-34-224-62-1.compute-1.amazonaws.com:8080')
 
-    imageToProcessFilePath = app.config['UPLOAD_FOLDER'] + '/' + filename
+    imageToProcessFilePath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
     with open(imageToProcessFilePath, "rb") as handle:
         binary_data = xmlrpc.client.Binary(handle.read())
         filterList = ["noise","contrast"]
         imageData = serverMethods.processImage(binary_data, fileType, filterList).data
 
-    processedImageFilePath = app.config['PROCESSED_FOLDER'] + '/' + filename
+    processedImageFilePath = os.path.join(app.config['PROCESSED_FOLDER'], filename)
     with open(processedImageFilePath, "wb") as handle:
         handle.write(imageData)
 
-    return ("Sucess")
+    displayProcessedImgFilePath = os.path.join(app.config['TEMPLATES_FOLDER'], filename)
+    with open(displayProcessedImgFilePath, "wb") as handle:
+        handle.write(imageData)
+
+    print(displayProcessedImgFilePath)
+    return render_template('interface.html', filename = filename)
+
+@app.route('/static/<filename>')
+def displayFile(filename):
+    return send_from_directory(TEMPLATES_FOLDER, filename)
+
+# @app.context_processor
+# def override_url_for():
+#     return dict(url_for=dated_url_for)
+
+# def dated_url_for(endpoint, **values):
+#     if endpoint == 'static':
+#         filename = values.get('filename', None)
+#         if filename:
+#             file_path = os.path.join(app.root_path,
+#                                  endpoint, filename)
+#             values['q'] = int(os.stat(file_path).st_mtime)
+#     return url_for(endpoint, **values)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
+
+app.debug = True
+app.run()
